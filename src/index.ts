a910417c6e733f042f1784b6fb73f52c336b6a62
Trifall <JerrenT@Trifall.com>
2023-09-30 01:54:11 -0400

    initial routes with examples

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log('Hello via Bun test!');
+import cors from '@elysiajs/cors';
+import swagger from '@elysiajs/swagger';
+import Elysia from 'elysia';
+import router from './routes/router';
+
+console.log('Hello bunbert');
+
+const app = new Elysia().use(swagger()).use(cors()).use(router).listen(8080);
+
+console.log(`🦊 Elysia is running at on port ${app.server?.port}...`);
